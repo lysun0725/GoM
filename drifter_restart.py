b1e731906996e4628e6b1b30c42d183ae35e9198
Luyu SUN <lysun@wireless-10-104-117-104.umd.edu>
2018-12-07 14:14:24 -0500

    add drifter restart file

---
+++ drifter_restart.py
@@ -0,0 +1,46 @@
+import netCDF4 as nc
+import numpy as np
+
+def createParticlesNC(fnam,ids,ines,jnes,lons,lats,levs,days):
+    f=nc.Dataset(fnam,'w',format='NETCDF3_CLASSIC',clobber=True)
+    idim=f.createDimension('i',None)
+    ivv=f.createVariable('i',np.float64)
+    iv=f.createVariable('drifter_num',np.int32,('i',))
+    inev=f.createVariable('ine',np.int32,('i',))
+    jnev=f.createVariable('jne',np.int32,('i',))
+    latv=f.createVariable('lat',np.float64,('i',))
+    lonv=f.createVariable('lon',np.float64,('i',))
+    levv=f.createVariable('depth',np.float64,('i',))
+    dv=f.createVariable('time',np.float64,('i',))
+
+    f.file_format_major_version=1
+    f.file_format_minor_version=1
+    iv.long_name='identification of the drifter'
+    iv.units='dimensionless'
+    iv.packing=0
+    inev.long_name='i index'
+    inev.units='none'
+    inev.packing=0
+    jnev.long_name='j index'
+    jnev.units='none'
+    jnev.packing=0
+    lonv.long_name='longitude'
+    lonv.units='degrees_east'
+    latv.long_name='latitude'
+    latv.units='degrees_north'
+    levv.long_name= 'depth below surface'
+    levv.units='m'
+    dv.units='days since 1900-01-01 00:00:00'
+    ivv[:]=len(ids[:])
+    iv[:] = ids[:]
+    inev[:]=ines[:]
+    jnev[:]=jnes[:]
+    lonv[:]=lons[:]
+    latv[:]=lats[:]
+    levv[:]=levs[:]
+    dv[:]=days[:]
+    f.sync()
+    f.close()
+    
+    
+createParticlesNC('drifters.res.nc',ids=[1,2,3],ines=[0,0,0],jnes=[0,0,0],lons=[270.1,270.3,270.6],lats=[24.625,24.375,24.875],levs=[15, 15, 15],days=[1,1,1])
